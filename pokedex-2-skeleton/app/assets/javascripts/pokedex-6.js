73af19f61cbe4a4f4dae85e0268950d6137b59c6
Wai + Ari <wario@appacademy.io>
2015-07-07 17:49:34 -0400

    It's working somehow.

--- pokedex-2-skeleton/app/assets/javascripts/pokedex-6.js
+++ pokedex-2-skeleton/app/assets/javascripts/pokedex-6.js
@@ -5,15 +5,39 @@ Pokedex.Router = Backbone.Router.extend({
   },
 
   pokemonDetail: function (id, callback) {
-    console.log(id);
+    // if (!this._pokemonIndex) {
+    //   this.pokemonIndex(this.pokemonDetail.bind(this, id, callback));
+    //   return;
+    // }
+
+    // var pokemon = this._pokemonIndex.collection.get(id);
+    //
+    // this._pokemonDetail =
+    //     new Pokedex.Views.PokemonDetail({ model: pokemon });
+    // $("#pokedex .pokemon-detail").html(this._pokemonDetail.$el);
+    // this._pokemonDetail.refreshPokemon({ success: callback });
+    if (!this._pokemonIndex){
+      this.pokemonIndex(this.pokemonDetail.bind(this, id, callback));
+    }
+
+    var pokemon = this._pokemonIndex.collection.get(id);
+
+    this._pokemonDetail = new Pokedex.Views.PokemonDetail ({
+        model: pokemon,
+      });
+
+    $("#pokedex .pokemon-detail").html(this._pokemonDetail.$el)
+
+    this._pokemonDetail.refreshPokemon({ success: callback });
   },
 
   pokemonIndex: function (callback) {
-    $(function () {
-      var pokemonIndex = new Pokedex.Views.PokemonIndex();
-      pokemonIndex.refreshPokemon();
-      $("#pokedex .pokemon-list").html(pokemonIndex.$el);
+    this._pokemonIndex = new Pokedex.Views.PokemonIndex();
+    $("#pokedex .pokemon-list").html(this._pokemonIndex.$el);
+    this._pokemonIndex.refreshPokemon({
+      success: callback
     });
+    // this.pokemonForm();
   },
 
   toyDetail: function (pokemonId, toyId) {
