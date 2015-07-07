aee238c0825e5990c40a38d2df664723ba1e39f4
Wai + Ari <wario@appacademy.io>
2015-07-07 13:58:17 -0400

    Get toy detail list to show on click

--- pokedex-2-skeleton/app/assets/javascripts/pokedex-2.js
+++ pokedex-2-skeleton/app/assets/javascripts/pokedex-2.js
@@ -5,35 +5,25 @@ Pokedex.RootView.prototype.addToyToList = function (toy) {
 
 Pokedex.RootView.prototype.renderToyDetail = function (toy) { // III
   this.$toyDetail.empty();
-
-  var $detail = $('<div class="detail">');
-  $detail.append('<img src="' + toy.escape('image_url') + '"><br>');
-  for (var attr in toy.attributes) {
-    if(attr !== 'pokemon_id' && attr !== 'image_url') {
-      var $span = $('<span style="font-weight:bold;">');
-      $span.html(attr + ': ');
-      $detail.append($span);
-      $detail.append(toy.get(attr));
-      $detail.append('<br>');
-    }
-  }
+  var content = JST["toyDetail"]( {toy: toy} );
+  this.$toyDetail.html(content);
 
   // Phase III
-  var $pokemonSelect = $('<select>');
-  $pokemonSelect.data("pokemon-id", toy.get("pokemon_id"));
-  $pokemonSelect.data("toy-id", toy.id);
-  this.pokes.each(function (pokemon) {
-    var $pokemonOption = $('<option>');
-    $pokemonOption.attr("value", pokemon.id);
-    $pokemonOption.text(pokemon.get("name"));
-    if (pokemon.id == toy.get("pokemon_id")) {
-      $pokemonOption.prop("selected", true);
-    }
-    $pokemonSelect.append($pokemonOption);
-  });
-  $detail.append($pokemonSelect);
+  // var $pokemonSelect = $('<select>');
+  // $pokemonSelect.data("pokemon-id", toy.get("pokemon_id"));
+  // $pokemonSelect.data("toy-id", toy.id);
+  // this.pokes.each(function (pokemon) {
+  //   var $pokemonOption = $('<option>');
+  //   $pokemonOption.attr("value", pokemon.id);
+  //   $pokemonOption.text(pokemon.get("name"));
+  //   if (pokemon.id == toy.get("pokemon_id")) {
+  //     $pokemonOption.prop("selected", true);
+  //   }
+  //   $pokemonSelect.append($pokemonOption);
+  // });
+  // $detail.append($pokemonSelect);
 
-  this.$toyDetail.html($detail);
+  // this.$toyDetail.html($detail);
 };
 
 Pokedex.RootView.prototype.selectToyFromList = function (event) {
